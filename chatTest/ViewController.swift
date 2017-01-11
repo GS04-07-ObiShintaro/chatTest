fe3b5dbc0a3e26b56cf7129c7d9f9e5f47c90480
小尾真太郎 <shinta5486@gmail.com>
2017-01-11 17:00:17 +0900

    とりあえず時刻も表示（表示の仕方はこれから）

--- chatTest/ViewController.swift
+++ chatTest/ViewController.swift
@@ -5,7 +5,8 @@ import FirebaseDatabase
 
 class ViewController: JSQMessagesViewController {
    var messages = [JSQMessage]()
-    
+   var mesDate : [String?] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
         senderDisplayName = "A"
@@ -30,11 +31,9 @@ class ViewController: JSQMessagesViewController {
 //            self.collectionView.reloadData()
 //        })
         
-        var formatter = DateFormatter()
-        let jaLocale = Locale(identifier: "ja_JP")
-        formatter.locale = jaLocale
-        formatter.dateStyle = .short
-        formatter.timeStyle = .short
+        let formatter = DateFormatter()
+        formatter.locale = NSLocale(localeIdentifier: "ja_JP") as Locale!
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
         
         let ref = FIRDatabase.database().reference().child("messages")
         ref.observe(.childAdded, with: { snapshot in
@@ -45,10 +44,10 @@ class ViewController: JSQMessagesViewController {
             let senderId = dic["senderId"]
             let text = dic["text"]
             let displayName = dic["displayName"]
-//            let time = formatter.date(from: dic["date"]!)
-//            let nowtime = Date()
             let mes = JSQMessage(senderId: senderId, displayName: displayName,  text: text)
+            //let mes2 = JSQMessage(senderId: senderId, senderDisplayName: displayName, date: Date(), text: text)
             self.messages.append(mes!)
+            self.mesDate.append(dic["date"]!)
             self.collectionView.reloadData()
         })
     }
@@ -79,11 +78,47 @@ class ViewController: JSQMessagesViewController {
         return (self.messages.count)
     }
     
+
+//    override func collectionView(_ collectionView: JSQMessagesCollectionView!, attributedTextForCellBottomLabelAt indexPath: IndexPath!) -> NSAttributedString!{
+//        //print(":::\(indexPath.row)")
+//        print(":::1")
+//        return test2
+//    }
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, attributedTextForCellTopLabelAt indexPath: IndexPath!) -> NSAttributedString!{
+        let Date = mesDate[indexPath.row]
+        return NSAttributedString(string: Date!)
+
+    }
+//    override func collectionView(_ collectionView: JSQMessagesCollectionView!, attributedTextForMessageBubbleTopLabelAt indexPath: IndexPath!) -> NSAttributedString!{
+//         print(":::3")
+//        return test2
+//    }
+    
+
+    
+    let testCG = CGFloat(20.0)
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, layout collectionViewLayout: JSQMessagesCollectionViewFlowLayout!, heightForCellTopLabelAt indexPath: IndexPath!) -> CGFloat{
+        return testCG
+    }
+//    override func collectionView(_ collectionView: JSQMessagesCollectionView!, layout collectionViewLayout: JSQMessagesCollectionViewFlowLayout!, heightForMessageBubbleTopLabelAt indexPath: IndexPath!) -> CGFloat{
+//        print(":::9")
+//        return testCG
+//    }
+    
+//↓ボトム
+//    override func collectionView(_ collectionView: JSQMessagesCollectionView!, layout collectionViewLayout: JSQMessagesCollectionViewFlowLayout!, heightForCellBottomLabelAt indexPath: IndexPath!) -> CGFloat{
+//        return testCG
+//    }
+    
+    
+    
     //送信ボタンを押したとき,データベースにコメントを保存
     override func didPressSend(_ button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: Date!){
         inputToolbar.contentView.textView.text = ""
-        let nowtime = String(describing: date!)
-        let mes = ["senderId": senderId!, "text": text!, "displayName": senderDisplayName!, "date": nowtime]
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy年MM月dd日 HH時mm分"
+        let dateString:String = formatter.string(from: date)
+        let mes = ["senderId": senderId!, "text": text!, "displayName": senderDisplayName!, "date": dateString]
         let ref = FIRDatabase.database().reference()
         ref.child("messages").childByAutoId().setValue(mes)
     }
